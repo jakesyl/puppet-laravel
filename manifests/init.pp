794f5a1885b6f3cc73cefde90af6691b4e21c272
David Hopson <admin@darkmantle.co.uk>
2015-04-25 03:22:24 +0100

    Fixed lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,24 +6,24 @@
 #
 # Actions:
 #
-# Requires: see Modulefile
+# Requires: see metadata.json
 #
 # Sample Usage:
 #
 class laravel {
-  
   # empty webroot
-  file {"/vagrant/webroot":
-    ensure => directory,
+  file { '/vagrant/webroot':
+    ensure  => directory,
     recurse => true,
-    purge => true,
-    force => true
+    purge   => true,
+    force   => true
   }
+
   # create new project
-  exec {"create_project":
-    command => "composer create-project laravel/laravel --prefer-dist .",
-    cwd => "/vagrant/webroot",
-    onlyif => "test -f /usr/bin/composer",
-    require => [File["/vagrant/webroot"],Package["php5-mcrypt"]]
+  exec { 'create_project':
+    command => 'composer create-project laravel/laravel --prefer-dist .',
+    cwd     => '/vagrant/webroot',
+    onlyif  => 'test -f /usr/bin/composer',
+    require => [File['/vagrant/webroot'], Package['php5-mcrypt']]
   }
 }
